6f6300db1c66e32d43f23d577d4fd27e2bfe5cab
Eesha Iyer <154126381+eesha2712@users.noreply.github.com>
2025-03-31 18:50:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 window.scrollTo({
                     top: targetSection.offsetTop - 50,
                     behavior: "smooth"
-                    <script src="script.js"></script>
-
                 });
             }
         });
